18168e5892a1d4e7595c82036f6776c7162a1c39
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2012-01-23 15:53:26 +0100

    Correct date in license disclaimer

--- src/libpco.h
+++ src/libpco.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010, 2011 Karlsruhe Institute of Technology
+/* Copyright (C) 2011, 2012 Karlsruhe Institute of Technology
 
    This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as published by the
