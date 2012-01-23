bd650a50404460c44f1ada3cebdeb1dc8698957c
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2012-01-23 16:21:37 +0100

    Adjust disclaimer header

--- src/libpco.h
+++ src/libpco.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 2011, 2012 Karlsruhe Institute of Technology
+/* Copyright (C) 2011, 2012 Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
+   (Karlsruhe Institute of Technology)
 
    This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as published by the
