d26fa0842ba25d6f68f1eeab4df86097efa3179c
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2012-02-28 12:07:58 +0100

    Fix call

--- test/main.c
+++ test/main.c
@@ -231,7 +231,7 @@ int main(int argc, char const* argv[])
     printf(" ROI: <%i,%i> to <%i,%i>\n", roi_window[0], roi_window[1], roi_window[2], roi_window[3]);
     printf(" Dimensions: %ix%i\n", width_std, height_std);
 
-    CHECK_PCO(pco_get_possible_binning(pco, NULL, NULL, NULL, NULL));
+    CHECK_PCO(pco_get_possible_binnings(pco, NULL, NULL, NULL, NULL));
 
     uint32_t width = width_std, height = height_std;
 
